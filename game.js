5bed1682cea0ad0286229cdf48aef407b1e357b5
April Ross <aprilross@Aprils-MacBook-Pro.local>
2017-06-25 09:53:44 -0400

    revision3

--- game.js
+++ game.js
@@ -24,16 +24,25 @@ app.set("views","./views");
 app.set("view engine", "mustache");
 
 
-var randomIndex = Math.floor(Math.random() * words.length);
-var randomWord = words[randomIndex];
+//var randomIndex = Math.floor(Math.random() * words.length);
+//var randomWord = words[randomIndex];
 
-console.log(randomWord);
+var randomWord = "apples";
+var randomWord_letters = randomWord.split("");
+//console.log(randomWord);
+//console.log(randomWord_letters);
 
-//creating dashes for each letter in randomWord
+//game pieces
+var attempts = 8; //establishing the limit of 8 attempts
 var dashedWords = {};
+var userLetters = "";
+var correctLetters = {};
+var wrongLetters = {};
+var gameOver = {};
 
- var guessedLetters = {};
- var dashes = "";
+//console.log(gameOver);
+
+var dashes = "";
 for(var i =0; i < randomWord.length; i++){
   if (randomWord.charAt(i) == " ") {
    dashes += " ";
@@ -42,15 +51,48 @@ for(var i =0; i < randomWord.length; i++){
  }
 }
 dashedWords.count = dashes;
-console.log(dashedWords);
+userLetters = dashes;
+//console.log(userLetters);
+//console.log(dashedWords);
 
 app.get("/game", function(req, res){
-  //setting up render "name of file to render", data to send to it
-  console.log(req.body);
-  res.render("game", {dashName:dashes});
+  //setting up render "name of file to render", {object:data to send to endpoint}
+  res.render("game", {dashedWords:dashes});
+
 });
 
+let messages = [];
+app.post("/game", function(req,res){
+  req.checkBody("letters", "Please enter one letter at a time").isLength({max: 1});
+  req.checkBody("letters", "Please enter a letter.").notEmpty();
+
+  let errors = req.validationErrors();
+  if (errors){
+    errors.forEach(function (error){  //loop thru each entry
+     messages.push(error.msg);         //display error message in the array
+    });
+      res.render("/game",{error:messages, dashedWords:dashes});
+  } else if (!errors && attempts > 0) { //if no errors, compare user entry to letters in randomWord
+    for (var j = 0; j < randomWord_letters; j++){
+      if(randomWord_letters[j] !== req.body.letters){
+        wrongLetters.push(req.body.letters);
+        res.render("/game", {dashedWords:dashes, wrongLetters:req.body.letters});
+        attempts = attempts - 1;
+      }
+    }
+  } else if (!errors && attempts > 0) {
+     for (var n = 0; n < randomWord_letters.length; n++){
+       if(randomWord_letters[n] === req.body.letters){
+         dashes = userLetters.replace(dashes[n], req.body.letters);
+           res.render("/game",{dashedWords:dashes});
+          }
+     }
+  } else {
+  gameOver.msg = "You lose!";
+  res.render("/game", {gameOver:"You lose!"});
+}
+});
 
 app.listen(3000, function (){
-console.log("App is running");
+console.log("App is running on port 3000");
 });
